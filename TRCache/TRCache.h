5662627b692e8561efee3ed819172964341fe4e1
joshua li <joshuali@joshuadeMac-mini.local>
2015-09-09 18:40:31 +0800

    spec

---
+++ TRCache/TRCache.h
@@ -0,0 +1,13 @@
+//
+//  TRCache.h
+//  TRCache
+//
+//  Created by joshua li on 15/9/9.
+//  Copyright (c) 2015年 joshua li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRCache : NSObject
+
+@end
---
+++ TRCache/TRCache.m
@@ -0,0 +1,13 @@
+//
+//  TRCache.m
+//  TRCache
+//
+//  Created by joshua li on 15/9/9.
+//  Copyright (c) 2015年 joshua li. All rights reserved.
+//
+
+#import "TRCache.h"
+
+@implementation TRCache
+
+@end
